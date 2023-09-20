a5041e826737aefb93cc0a7d11b685417dec64ba
iy811 <iy8116@gmail.com>
2023-09-20 19:24:30 +0100

    continued nested if statement

--- day01/is_statements.py
+++ day01/is_statements.py
@@ -53,13 +53,21 @@ result = None
 
 if num > 0 :
     result = 'Positive'
-elif num < 0:
+elif num < 0:   # same with else if block Java
     result = 'Negative'
 else:
     result = 'Zero'
 
 print(result)
 # Ternary can not be USED for multi-branch if-statement, it can only be used if-else statement
+
+print('-------------------------------------------------')
+
+num = 200
+
+result2 = 'Positive' if num > 0 else 'Zero'
+
+print(result2)
 
 print('-------------------------------------------------')
 
@@ -71,3 +79,28 @@ if 0 <= score <= 100:
         print('Failed')
 else :
     print('Invalied score')
+
+print('-------------------------------------------------')
+
+"""
+A score of a student is given, write a program that can calculate the grade of the student
+
+"""
+
+grade = 79
+
+if 0 <= grade <= 100:
+
+    if grade >=90:
+        print ("Your grade is A")
+    elif grade >=80:
+        print("Your grade is B")
+    elif grade >=70:
+        print("Your grade is C")
+    elif grade >=60:
+        print("Your grade is D")
+    else:
+        print("Your grade is F")
+else :
+    print('Invalid grade')
+
